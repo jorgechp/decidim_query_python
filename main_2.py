ce22a5ce4f9c7e2c271f4dd7bc070dfd2c83658b
jorgechp <jorgechp@yandex.com>
2021-03-12 00:35:40 +0100

    Prepare Pypi package

--- main_2.py
+++
@@ -1,8 +0,0 @@
-from api.complex.get_all_comments_from_proposals_reader import GetAllCommentsFromProposalsReader
-from api.decidim_connector import DecidimConnector
-
-API_URL = "https://meta.decidim.org/api"
-
-decidim_connector = DecidimConnector(API_URL)
-all_comments_reader = GetAllCommentsFromProposalsReader(decidim_connector)
-all_comments_reader.execute(depth=6)
